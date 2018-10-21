bc0931ae321b06c7306fcaa4bc5dbceb22107658
Grauero <grayero6996@gmail.com>
2018-10-21 17:00:04 +0300

    Added routes for getting user profiles by Handle and ID, and route for getting all user profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -25,8 +25,74 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
     .catch(err => res.status(404).json(err));
 });
 
+// @route   GET api/profile/all
+// @desc    Get all profiles
+// @access  Public
+router.get('/all', (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then((profiles) => {
+      if (!profiles) {
+        errors.noProfiles = 'There are no profiles';
+        res.status(404).json(errors);
+      }
+
+      res.json(profiles);
+    })
+    .catch(() => {
+      errors.handle = 'Internal server error';
+      return res.status(500).json(errors);
+    });
+});
+
+// @route   GET api/profile/handle/:handle
+// @desc    Get profile by handle
+// @access  Public
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'avatar'])
+    .then((profile) => {
+      if (!profile) {
+        errors.noProfile = 'Profile doesnt exist';
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(() => {
+      errors.handle = 'Internal server error';
+      return res.status(500).json(errors);
+    });
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user id
+// @access  Public
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then((profile) => {
+      if (!profile) {
+        errors.noProfile = 'Profile doesnt exist';
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(() => {
+      errors.handle = 'Internal server error';
+      return res.status(500).json(errors);
+    });
+});
+
 // @route   POST api/profile
-// @desc    Create or update users profile
+// @desc    Create or update user profile
 // @access  Private
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
   const { errors, isValid } = validateProfileInput(req.body);
@@ -86,7 +152,7 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
         new Profile(profileFields).save()
           .then(profile => res.json(profile))
           .catch(() => {
-            errors.handle = 'Create error';
+            errors.handle = 'Creation error';
             return res.status(500).json(errors);
           });
       });
