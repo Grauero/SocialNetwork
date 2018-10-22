17db54962246f2cc86f3fbb150ad71dc6ac4b25b
Grauero <grayero6996@gmail.com>
2018-10-22 17:04:15 +0300

    Added route for create post and validation for it

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const passport = require('passport');
+
+const Post = require('../../models/Post');
+const validatePostInput = require('../../validation/post');
 
 const router = express.Router();
 
-router.get('/test', (req, res) => res.json({ msg: 'Posts works' }));
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+
+  // check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const { text, name, avatar } = req.body;
+  const newPost = new Post({
+    text, name, avatar, user: req.user.id
+  });
+  newPost.save()
+    .then((post) => {
+      res.json(post);
+    })
+    .catch(() => {
+      errors.handle = 'Post doesnt exist';
+      return res.status(404).json(errors);
+    });
+});
 
 module.exports = router;
