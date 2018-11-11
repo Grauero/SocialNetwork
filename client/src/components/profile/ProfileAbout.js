bb7f80aceac990cecb6c58279370d5442e1565dc
Grauero <grayero6996@gmail.com>
2018-11-11 17:14:50 +0300

    Created ProfileAbout, ProfileCreds components

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
+import propTypes from 'prop-types';
 
-class ProfileAbout extends Component {
-  render() {
-    return (
-      <div>
-        About
+import isEmpty from '../../validation/isEmpty';
+
+const ProfileAbout = ({ profile }) => {
+  const firstName = profile.user.name.trim().split(' ')[0];
+  const skills = profile.skills.map(skill => (
+    <div key={skill} className="p-3">
+      <i className="fa fa-check" /> {skill}
+    </div>
+  ));
+
+  return (
+    <div className="row">
+      <div className="col-md-12">
+        <div className="card card-body bg-light mb-3">
+          <h3 className="text-center text-info">{`${firstName}'s Bio`}</h3>
+          <p className="lead">
+            {
+                isEmpty(profile.bio)
+                  ? <span>{firstName} doesnt have a bio</span>
+                  : <span>{profile.bio}</span>
+              }
+          </p>
+          <hr />
+          <h3 className="text-center text-info">Skill Set</h3>
+          <div className="row">
+            <div className="d-flex flex-wrap justify-content-center align-items-center">
+              {skills}
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+ProfileAbout.propTypes = {
+  profile: propTypes.instanceOf(Object).isRequired
+};
 
 export default ProfileAbout;
