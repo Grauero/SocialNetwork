130b2faff2ab0d09dac2adfff2a192fa11136b63
Grauero <grayero6996@gmail.com>
2018-11-20 22:11:20 +0300

    Added config for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
@@ -26,6 +27,14 @@ applyPassportStrategies(passport);
 app.use('/api/posts', posts);
 app.use('/api/profile', profile);
 app.use('/api/users', users);
+
+// Static assets for production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+} 
 
 const port = process.env.PORT || 80;
 const mode = process.env.NODE_ENV === 'test' ? 'test' : 'dev';
