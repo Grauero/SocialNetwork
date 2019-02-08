8608f3937eb723530f101498fd2831e67d2831a2
Grauero <grayero6996@gmail.com>
2019-02-08 16:41:41 +0300

    Fix env variables for production

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,7 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/api', { target: 'http://localhost:80' }));
+  if (process.env.REACT_ENV === 'dev') {
+    app.use(proxy('/api', { target: 'http://localhost:80' }));
+  }
 };
